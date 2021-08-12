dfcd6553e41ef41e592923f6415dce22da644a15
Documenter.jl <documenter@juliadocs.github.io>
2021-08-12 14:07:57 +0000

    build based on e6676b6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuralOperators","category":"page"},{"location":"#NeuralOperators","page":"Home","title":"NeuralOperators","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralOperators.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuralOperators]","category":"page"}]
+}
