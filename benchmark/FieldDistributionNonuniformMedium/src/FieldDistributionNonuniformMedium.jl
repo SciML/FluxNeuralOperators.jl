378d934d7a63ede3e7bd76d6679e1275a542ee43
Yueh-Hua Tu <a504082002@gmail.com>
2022-02-05 17:20:48 +0800

    refactor into GridParameter

--- benchmark/FieldDistributionNonuniformMedium/src/FieldDistributionNonuniformMedium.jl
+++ benchmark/FieldDistributionNonuniformMedium/src/FieldDistributionNonuniformMedium.jl
@@ -6,32 +6,46 @@ using JLD2
 
 const C = 299792458
 
-struct Bound
-    max_x::Float64
-    max_y::Float64
-    max_t::Float64
+struct GridParameter{T<:Real}
+    nx::Int
+    ny::Int
+    nt::Int
+
+    Δx::T
+    Δy::T
+    Δt::T
+
+    max_x::T
+    max_y::T
+    max_t::T
 end
 
-struct Discretizer
-    nx::Int64
-    ny::Int64
-
-    Δx::Float64
-    Δy::Float64
-
-    Δt::Float64
-    nt::Int64
-end
-
-function Discretizer(nx, ny, b::Bound)
-    Δx = b.max_x / nx
-    Δy = b.max_y / ny
+function discretize(nx, ny; max_x=3e-6, max_y=10e-6, max_t=5e-12)
+    Δx = max_x / nx
+    Δy = max_y / ny
 
     Δt = 1 / C / √(1/Δx^2 + 1/Δy^2)
-    nt = round(Int, b.max_t/Δt)
+    nt = round(Int, max_t/Δt)
 
-    return Discretizer(nx, ny, Δx, Δy, Δt, nt)
+    return GridParameter(nx, ny, nt, Δx, Δy, Δt, max_x, max_y, max_t)
 end
+
+function init(gp::GridParameter)
+    Δx, Δy, nx, ny = gp.Δx, gp.Δy, gp.nx, gp.ny
+
+    return cat(
+        repeat(Δx * ((1:nx) .- nx/2), 1, ny, 1),
+        repeat(Δy * (transpose(1:ny) .- Δy/2), nx, 1, 1),
+        dims=3
+    )
+end
+
+Base.size(gp::GridParameter) = (gp.nx, gp.ny)
+Base.size(gp::GridParameter, d) = d::Integer <= 2 ? size(gp)[d] : 1
+Base.axes(gp::GridParameter) = (Base.OneTo(gp.nx), Base.OneTo(gp.ny))
+Base.axes(gp::GridParameter, d) = d::Integer <= 2 ? axes(gp)[d] : 1
+boundary(gp::GridParameter) = (gp.max_x, gp.max_y)
+boundary(gp::GridParameter, d) = d::Integer <= 2 ? boundary(gp)[d] : 1
 
 struct Light{T<:Real}
     λ::T
@@ -48,23 +62,23 @@ struct Permittivity{T<:AbstractMatrix}
     ϵy::T
 end
 
-function Base.rand(::Type{Permittivity}, n::Integer, r::Float64, b::Bound, d::Discretizer)
-    ϵ = 9 * ones(d.nx, d.ny)
+function Base.rand(::Type{Permittivity}, n::Integer, r::Real, gp::GridParameter)
+    ϵ = 9 * ones(size(gp))
 
-    xs = b.max_x .* rand(n)
-    ys = b.max_y .* rand(n)
+    xs = gp.max_x .* rand(n)
+    ys = gp.max_y .* rand(n)
     rs = r .* rand(n)
 
     in_circle(i, j) = true in [
-        √((i*d.Δx - xs[c])^2 + (j*d.Δy - ys[c])^2) < rs[c] for c in 1:n
+        √((i*gp.Δx - xs[c])^2 + (j*gp.Δy - ys[c])^2) < rs[c] for c in 1:n
     ]
 
-    for i in 1:d.nx, j in 1:d.ny
+    for i in 1:gp.nx, j in 1:gp.ny
         in_circle(i, j) && (ϵ[i, j] = 1)
     end
 
-    ϵx = C * d.Δt/d.Δx ./ ϵ
-    ϵy = C * d.Δt/d.Δy ./ ϵ
+    ϵx = C * gp.Δt/gp.Δx ./ ϵ
+    ϵy = C * gp.Δt/gp.Δy ./ ϵ
 
     return Permittivity(ϵ, ϵx, ϵy)
 end
@@ -75,16 +89,15 @@ struct Permeability{T<:Real}
     μy::T
 end
 
-function Permeability(μ, d::Discretizer)
-    μx = C * d.Δt/d.Δx / μ
-    μy = C * d.Δt/d.Δy / μ
+function Permeability(μ, gp::GridParameter)
+    μx = C * gp.Δt/gp.Δx / μ
+    μy = C * gp.Δt/gp.Δy / μ
 
     return Permeability(μ, μx, μy)
 end
 
 mutable struct Simulator
-    bound::Bound
-    discretizer::Discretizer
+    grid::GridParameter
     light::Light
     permittivity::Permittivity
     permeability::Permeability
@@ -103,38 +116,36 @@ function Simulator(;
     n=rand(1:5), r=0.45e-6,
     μ=1.
 )
-    bound = Bound(max_x, max_y, max_t)
-    discretizer = Discretizer(nx, ny, bound)
+    gp = discretize(nx, ny, max_x=max_x, max_y=max_y, max_t=max_t)
     light = Light(λ)
-    permittivity = rand(Permittivity, n, r, bound, discretizer)
-    permeability = Permeability(μ, discretizer)
+    permittivity = rand(Permittivity, n, r, gp)
+    permeability = Permeability(μ, gp)
 
-    Δx, Δt = discretizer.Δx, discretizer.Δt
+    Δx, Δt = gp.Δx, gp.Δt
 
-    ez = zeros(Float64, nx, ny)
+    ez = zeros(Float64, size(gp))
     ez[2:nx, 1] .= 0.1exp.(
         -(Δx * ((2:nx) .- nx/2)).^2 ./
         (max_x/4)^2
     ) * sin(light.k * C*Δt)
 
     return Simulator(
-        bound,
-        discretizer,
+        gp,
         light,
         permittivity,
         permeability,
 
         ez,
-        zeros(Float64, nx, ny),
-        zeros(Float64, nx, ny),
+        zeros(Float64, size(gp)),
+        zeros(Float64, size(gp)),
 
         0
     )
 end
 
 function next!(s::Simulator)
-    max_x = s.bound.max_x
-    nx, ny, Δx, Δt = s.discretizer.nx, s.discretizer.ny, s.discretizer.Δx, s.discretizer.Δt
+    nx, ny = size(s.grid)
+    Δx, Δt, max_x = s.grid.Δx, s.grid.Δt, s.grid.max_x
     k = s.light.k
     ϵx, ϵy = s.permittivity.ϵx, s.permittivity.ϵy
     μx, μy = s.permeability.μx, s.permeability.μy
@@ -157,32 +168,32 @@ function next!(s::Simulator)
 end
 
 function simulate!(s::Simulator)
-    for _ in 1:(s.discretizer.nt)
+    for _ in 1:(s.grid.nt)
         next!(s)
     end
 
     return s
 end
 
-function plot_ϵ(s::Simulator; size=(350, 750), left_margin=-100px)
+function plot_ϵ(s::Simulator; figsize=(350, 750), left_margin=-100px)
     plotly()
 
-    max_x, max_y = s.bound.max_x, s.bound.max_y
-    nx, ny = s.discretizer.nx, s.discretizer.ny
+    max_x, max_y = boundary(s.grid)
+    nx, ny = size(s.grid)
     ϵ = s.permittivity.ϵ
 
     return heatmap(
 		LinRange(0, max_x, nx), LinRange(0, max_y, ny), ϵ',
 		color=:algae,
-		size=size, left_margin=left_margin
+		size=figsize, left_margin=left_margin
 	)
 end
 
-function plot_e_field(s::Simulator; size=(300, 750), left_margin=-100px)
+function plot_e_field(s::Simulator; figsize=(300, 750), left_margin=-100px)
     plotly()
 
-    max_x, max_y = s.bound.max_x, s.bound.max_y
-    nx, ny = s.discretizer.nx, s.discretizer.ny
+    max_x, max_y = boundary(s.grid)
+    nx, ny = size(s.grid)
     ez = s.ez
     ϵ = s.permittivity.ϵ
 
@@ -190,7 +201,7 @@ function plot_e_field(s::Simulator; size=(300, 750), left_margin=-100px)
     p = heatmap(
 		LinRange(0, max_x, nx), LinRange(0, max_y, ny), ez',
 		color=:coolwarm, clim=(-lim, lim), colorbar=false,
-		size=size, left_margin=left_margin
+		size=figsize, left_margin=left_margin
 	)
     lim_ϵ = maximum(abs.(ϵ))
     p = contour!(
@@ -200,16 +211,6 @@ function plot_e_field(s::Simulator; size=(300, 750), left_margin=-100px)
     )
 
     return p
-end
-
-function get_grid(s::Simulator)
-    Δx, Δy, nx, ny = s.discretizer.Δx, s.discretizer.Δy, s.discretizer.nx, s.discretizer.ny
-
-    return cat(
-        repeat(Δx * ((1:nx) .- nx/2), 1, ny, 1),
-        repeat(Δy * (transpose(1:ny) .- Δy/2), nx, 1, 1),
-        dims=3
-    )
 end
 
 function gen_data(; nx=60, ny=200, n=7000)
@@ -222,7 +223,7 @@ function gen_data(; nx=60, ny=200, n=7000)
             simulate!(s)
 
             xs[:, :, 1, i] .= s.permittivity.ϵ[2:(nx-1), 2:(ny-1)]
-            xs[:, :, 2:3, i] .= get_grid(s)[2:(nx-1), 2:(ny-1), :]
+            xs[:, :, 2:3, i] .= init(s.grid)[2:(nx-1), 2:(ny-1), :]
             ys[:, :, 1, i] .= s.ez[2:(nx-1), 2:(ny-1)]
 
             print("\e[H\e[2J");
