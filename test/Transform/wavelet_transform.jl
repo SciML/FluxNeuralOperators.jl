09d085ccc809b63e3eb77f54cfd8ce7edc781999
Yueh-Hua Tu <a504082002@gmail.com>
2022-03-26 23:36:25 +0800

    add WaveletTransform

--- test/Transform/wavelet_transform.jl
+++ test/Transform/wavelet_transform.jl
@@ -1,53 +1,30 @@
-@testset "SparseKernel" begin
+@testset "wavelet transform" begin
+    𝐱 = rand(30, 40, 50, 6, 7) # where ch == 6 and batch == 7
+
+    wt = WaveletTransform((3, 4, 5))
+
+    @test size(transform(wt, 𝐱)) == (30, 40, 50, 6, 7)
+    @test size(truncate_modes(wt, transform(wt, 𝐱))) == (3, 4, 5, 6, 7)
+    @test size(inverse(wt, truncate_modes(wt, transform(wt, 𝐱)))) == (3, 4, 5, 6, 7)
+end
+
+@testset "MWT_CZ" begin
     T = Float32
     k = 3
     batch_size = 32
 
-    @testset "1D SparseKernel" begin
-        α = 4
-        c = 1
-        in_chs = 20
-        X = rand(T, in_chs, c*k, batch_size)
+    @testset "MWT_CZ1d" begin
+        mwt = MWT_CZ1d()
 
-        l1 = SparseKernel1D(k, α, c)
-        Y = l1(X)
-        @test l1 isa SparseKernel{1}
-        @test size(Y) == size(X)
+        # base functions
+        wavelet_transform(mwt, )
+        even_odd(mwt, )
 
-        gs = gradient(()->sum(l1(X)), Flux.params(l1))
-        @test length(gs.grads) == 4
+        # forward
+        Y = mwt(X)
+
+        # backward
+        g = gradient()
     end
 
-    @testset "2D SparseKernel" begin
-        α = 4
-        c = 3
-        Nx = 5
-        Ny = 7
-        X = rand(T, Nx, Ny, c*k^2, batch_size)
-    
-        l2 = SparseKernel2D(k, α, c)
-        Y = l2(X)
-        @test l2 isa SparseKernel{2}
-        @test size(Y) == size(X)
-
-        gs = gradient(()->sum(l2(X)), Flux.params(l2))
-        @test length(gs.grads) == 4
-    end
-
-    @testset "3D SparseKernel" begin
-        α = 4
-        c = 3
-        Nx = 5
-        Ny = 7
-        Nz = 13
-        X = rand(T, Nx, Ny, Nz, α*k^2, batch_size)
-
-        l3 = SparseKernel3D(k, α, c)
-        Y = l3(X)
-        @test l3 isa SparseKernel{3}
-        @test size(Y) == (Nx, Ny, Nz, c*k^2, batch_size)
-
-        gs = gradient(()->sum(l3(X)), Flux.params(l3))
-        @test length(gs.grads) == 4
-    end
 end
