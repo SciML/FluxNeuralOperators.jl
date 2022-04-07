3f8416a396f131929e58de844ac5d05f3e958113
JingYu Ning <foldfelis@gmail.com>
2022-04-07 17:04:26 +0800

    Update param name

--- src/loss.jl
+++ src/loss.jl
@@ -1,8 +1,8 @@
 export l₂loss
 
-function l₂loss(𝐲̂, 𝐲; agg=mean, is_grid_normalized=true)
+function l₂loss(𝐲̂, 𝐲; agg=mean, grid_normalize=true)
     feature_dims = 2:(ndims(𝐲)-1)
     loss = agg(.√(sum(abs2, 𝐲̂-𝐲, dims=feature_dims)))
 
-    return is_grid_normalized ? loss/prod(feature_dims) : loss
+    return grid_normalize ? loss/prod(feature_dims) : loss
 end
