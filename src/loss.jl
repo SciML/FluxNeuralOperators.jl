f2636dc6dbe3efef6ab6d9fbad8abae71783fa6b
JingYu Ning <foldfelis@gmail.com>
2022-04-09 16:33:54 +0800

    Refactor l2loss

--- src/loss.jl
+++ src/loss.jl
@@ -1,10 +1,10 @@
 export l₂loss
 
-function l₂loss(𝐲̂, 𝐲; agg=mean)
+function l₂loss(𝐲̂, 𝐲)
     feature_dims = 2:(ndims(𝐲)-1)
 
-    loss = agg(.√(sum(abs2, 𝐲̂-𝐲, dims=feature_dims)))
-    y_norm = agg(.√(sum(abs2, 𝐲, dims=feature_dims)))
+    loss = sum(.√(sum(abs2, 𝐲̂-𝐲, dims=feature_dims)))
+    y_norm = sum(.√(sum(abs2, 𝐲, dims=feature_dims)))
 
     return loss / y_norm
 end
