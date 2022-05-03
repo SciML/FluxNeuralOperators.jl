08de33b3d1e8b44d0b353718d632ce3e54c4747d
Yueh-Hua Tu <a504082002@gmail.com>
2022-05-03 23:23:18 +0800

    add gradient test for transformations
    fix

--- test/Transform/chebyshev_transform.jl
+++ test/Transform/chebyshev_transform.jl
@@ -8,4 +8,7 @@
     @test size(transform(t, 𝐱)) == (30, 40, 50, ch, batch)
     @test size(truncate_modes(t, transform(t, 𝐱))) == (3, 4, 5, ch, batch)
     @test size(inverse(t, truncate_modes(t, transform(t, 𝐱)))) == (3, 4, 5, ch, batch)
+
+    @test_broken g = gradient(x -> sum(inverse(t, truncate_modes(t, transform(t, x)))), 𝐱)
+    @test_broken size(g[1]) == (30, 40, 50, ch, batch)
 end
