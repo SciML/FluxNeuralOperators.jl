68b0f652845759c4f97efda47abe54eead4389b8
Venakteshprasad <32921645+ven-k@users.noreply.github.com>
2022-06-19 22:17:03 +0530

    Add NOMAD "Usage" and its introduction in docs

--- docs/src/introduction.md
+++ docs/src/introduction.md
@@ -24,3 +24,8 @@ by linking the operators into a Markov chain.
 Deep operator network (DeepONet) learns a neural operator with the help of two sub-neural network structures described as the branch and the trunk network.
 The branch network is fed the initial conditions data, whereas the trunk is fed with the locations where the target(output) is evaluated from the corresponding initial conditions.
 It is important that the output size of the branch and trunk subnets is same so that a dot product can be performed between them.
+
+## [Nonlinear Manifold Decoders for Operator Learning](https://github.com/SciML/NeuralOperators.jl/blob/master/src/NOMAD.jl)
+
+Nonlinear Manifold Decoders for Operator Learning (NOMAD) learns a neural operator with a nonlinear decoder parameterized by a deep neural network which jointly takes output of approximator and the locations as parameters.
+The approximator network is fed with the initial conditions data. The output-of-approximator and the locations are then passed to a decoder neural network to get the target (output). It is important that the input size of the decoder subnet is sum of size of the output-of-approximator and number of locations.
--- src/NOMAD.jl
+++ src/NOMAD.jl
@@ -17,6 +17,36 @@ arXiv:2206.03551
 The decoder is defined as follows:
 
 ``\\tilde D (β, y) = f(β, y)``
+
+# Usage
+
+```julia
+julia> model = NOMAD((16,32,16), (24,32))
+NOMAD with
+Approximator net: (Chain(Dense(16 => 32), Dense(32 => 16)))
+Decoder net: (Chain(Dense(24 => 32, true)))
+
+julia> model = NeuralOperators.NOMAD((32,64,32), (64,72), σ, tanh; init_approximator=Flux.glorot_normal, bias_decoder=false)
+NOMAD with
+Approximator net: (Chain(Dense(32 => 64, σ), Dense(64 => 32, σ)))
+Decoder net: (Chain(Dense(64 => 72, tanh; bias=false)))
+
+julia> approximator = Chain(Dense(2,128),Dense(128,64))
+Chain(
+  Dense(2 => 128),                      # 384 parameters
+  Dense(128 => 64),                     # 8_256 parameters
+)                   # Total: 4 arrays, 8_640 parameters, 34.000 KiB.
+
+julia> decoder = Chain(Dense(72,24),Dense(24,12))
+Chain(
+  Dense(72 => 24),                      # 1_752 parameters
+  Dense(24 => 12),                      # 300 parameters
+)                   # Total: 4 arrays, 2_052 parameters, 8.266 KiB.
+
+julia> model = NOMAD(approximator, decoder)
+NOMAD with
+Approximator net: (Chain(Dense(2 => 128), Dense(128 => 64)))
+Decoder net: (Chain(Dense(72 => 24), Dense(24 => 12)))
 """
 function NOMAD(architecture_approximator::Tuple, architecture_decoder::Tuple,
                 act_approximator = identity, act_decoder=true;
@@ -44,7 +74,7 @@ end
 
 # Print nicely
 function Base.show(io::IO, l::NOMAD)
-    print(io, "NOMAD with\napproximator net: (",l.approximator_net)
+    print(io, "NOMAD with\nApproximator net: (",l.approximator_net)
     print(io, ")\n")
     print(io, "Decoder net: (", l.decoder_net)
     print(io, ")\n")
