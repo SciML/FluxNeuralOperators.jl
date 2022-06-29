cf34d62d79d96ae039506dd53b8eda53f5a12432
Yueh-Hua Tu <a504082002@gmail.com>
2022-06-29 10:55:30 +0800

    fix GraphKernel

--- test/graph_kernel.jl
+++ test/graph_kernel.jl
@@ -1,16 +1,19 @@
 @testset "GraphKernel" begin
     batch_size = 5
     channel = 32
-    N = 10 * 10
+    coord_dim = 2
+    N = 10
 
-    κ = Dense(2 * channel, channel, relu)
+    graph = grid([N, N])
+    κ = Dense(2(coord_dim + 1), abs2(channel), relu)
 
-    graph = grid([10, 10])
-    𝐱 = rand(Float32, channel, N, batch_size)
+    𝐱 = rand(Float32, channel, nv(graph), batch_size)
+    E = rand(Float32, 2(coord_dim + 1), ne(graph), batch_size)
     l = WithGraph(FeaturedGraph(graph), GraphKernel(κ, channel))
-    @test repr(l.layer) == "GraphKernel(Dense(64 => 32, relu), channel=32)"
-    @test size(l(𝐱)) == (channel, N, batch_size)
+    @test repr(l.layer) ==
+          "GraphKernel(Dense($(2(coord_dim + 1)) => $(abs2(channel)), relu), channel=32)"
+    @test size(l(𝐱, E)) == (channel, nv(graph), batch_size)
 
-    g = Zygote.gradient(() -> sum(l(𝐱)), Flux.params(l))
+    g = Zygote.gradient(() -> sum(l(𝐱, E)), Flux.params(l))
     @test length(g.grads) == 3
 end
