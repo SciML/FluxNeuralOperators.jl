a012dc0268843d5f36fcdad21a8667ec85dbcaf5
JingYu Ning <foldfelis@gmail.com>
2022-07-06 18:43:07 +0800

    Enhanced docstring for model

--- src/model.jl
+++ src/model.jl
@@ -4,13 +4,81 @@ export
 
 """
     FourierNeuralOperator(;
-        ch=(2, 64, 64, 64, 64, 64, 128, 1),
-        modes=(16, ),
-        σ=gelu
-    )
+                          ch = (2, 64, 64, 64, 64, 64, 128, 1),
+                          modes = (16, ),
+                          σ = gelu)
 
-Fourier neural operator learns a neural operator with Dirichlet kernel to form a Fourier transformation.
-It performs Fourier transformation across infinite-dimensional function spaces and learns better than neural operator.
+Fourier neural operator is a operator learning model that uses Fourier kernel to perform
+spectral convolutions.
+It is a promissing way for surrogate methods, and can be regarded as a physics operator.
+
+The model is comprised of
+a `Dense` layer to lift (d + 1)-dimensional vector field to n-dimensional vector field,
+and an integral kernel operator which consists of four Fourier kernels,
+and two `Dense` layers to project data back to the scalar field of interest space.
+
+The role of each channel size described as follow:
+
+```
+[1] input channel number
+ ↓ Dense
+[2] lifted channel number
+ ↓ OperatorKernel
+[3] mapped cahnnel number
+ ↓ OperatorKernel
+[4] mapped cahnnel number
+ ↓ OperatorKernel
+[5] mapped cahnnel number
+ ↓ OperatorKernel
+[6] mapped cahnnel number
+ ↓ Dense
+[7] projected channel number
+ ↓ Dense
+[8] projected channel number
+```
+
+## Arguments
+
+* `ch`: A `Tuple` or `Vector` of the 8 channel size.
+* `modes`: The modes to be preserved. A tuple of length `d`,
+    where `d` is the dimension of data.
+* `σ`: Activation function for all layers in the model.
+
+## Example
+
+```julia
+julia> using NNlib
+
+julia> FourierNeuralOperator(;
+                             ch = (2, 64, 64, 64, 64, 64, 128, 1),
+                             modes = (16,),
+                             σ = gelu)
+Chain(
+  Dense(2 => 64),                       # 192 parameters
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    identity,
+  ),
+  Dense(64 => 128, gelu),               # 8_320 parameters
+  Dense(128 => 1),                      # 129 parameters
+)                   # Total: 18 arrays, 287_425 parameters, 2.098 MiB.
+```
 """
 function FourierNeuralOperator(;
                                ch = (2, 64, 64, 64, 64, 64, 128, 1),
@@ -29,14 +97,49 @@ end
 
 """
     MarkovNeuralOperator(;
-        ch=(1, 64, 64, 64, 64, 64, 1),
-        modes=(24, 24),
-        σ=gelu
+                         ch = (1, 64, 64, 64, 64, 64, 1),
+                         modes = (24, 24),
+                         σ  =gelu
     )
 
 Markov neural operator learns a neural operator with Fourier operators.
-With only one time step information of learning, it can predict the following few steps with low loss
-by linking the operators into a Markov chain.
+With only one time step information of learning, it can predict the following few steps
+with low loss by linking the operators into a Markov chain.
+
+## Example
+
+```julia
+julia> using NNlib
+
+julia> MarkovNeuralOperator(;
+                            ch = (2, 64, 64, 64, 64, 64, 128, 1),
+                            modes = (16,),
+                            σ = NeuralOperators.gelu)
+Chain(
+  Dense(2 => 64),                       # 192 parameters
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  OperatorKernel(
+    Dense(64 => 64),                    # 4_160 parameters
+    OperatorConv(64 => 64, (16,), FourierTransform, permuted=false),  # 65_536 parameters
+    NNlib.gelu,
+  ),
+  Dense(64 => 128),                     # 8_320 parameters
+)                   # Total: 16 arrays, 287_296 parameters, 2.097 MiB.
+```
 """
 function MarkovNeuralOperator(;
                               ch = (1, 64, 64, 64, 64, 64, 1),
