9b917c0f14de470f130b78cd1af3a8b3267fcb91
Avik Pal <avikpal@mit.edu>
2024-07-11 00:08:56 -0700

    test: more explicit imports testing

--- test/qa_tests.jl
+++ test/qa_tests.jl
@@ -18,5 +18,9 @@ end
     # Skip our own packages
     @test check_no_implicit_imports(NeuralOperators; skip=(Base, Core, Lux)) === nothing
     @test check_no_stale_explicit_imports(NeuralOperators) === nothing
+    @test check_no_self_qualified_accesses(NeuralOperators) === nothing
+    @test check_all_explicit_imports_via_owners(NeuralOperators) === nothing
     @test check_all_qualified_accesses_via_owners(NeuralOperators) === nothing
+    @test_broken check_all_explicit_imports_are_public(NeuralOperators) === nothing  # mostly upstream problems
+    @test_broken check_all_qualified_accesses_are_public(NeuralOperators) === nothing  # mostly upstream problems
 end
